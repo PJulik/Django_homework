19294fc56688f325d2bc782dfc8739f6ca84150f
Julia <julik6@mail.ru>
2024-01-17 14:43:52 +0300

    add changes

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from .views import SensorView, SensorUpdate, MeasurementView
 
 urlpatterns = [
+    path('sensors/', SensorView.as_view()),
+    path('sensors/<pk>/', SensorUpdate.as_view()),
+    path('measurements/<pk>/', MeasurementView.as_view()),
     # TODO: зарегистрируйте необходимые маршруты
-]
+ ]
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,27 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.response import Response
+
+from .models import Sensor, Measurement
+from .serializers import SensorSerializer, MeasurementSerializer, SensorDetailSerializer
+
+
+class SensorView(ListCreateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+class SensorUpdate(RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+class MeasurementView(CreateAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
+
+
+
+
+
+
